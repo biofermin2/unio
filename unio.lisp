21c7d9ad721568ced31609e0b4c1650ad9feb6d1
Higashi, Hiromitsu <supercaveman@gmail.com>
2021-11-21 00:48:59 +0900

    fixed seek , seek-files
    - seek : deal with both string and S-exp as list argument
    - seek-files : fixed format parameter for keyword

--- unio.lisp
+++ unio.lisp
@@ -7,26 +7,27 @@
 (defun seek (key list &optional (skin 0) (rm-dup t)
 		    &aux (depth 0) (key-len (length key)) pos)
   (check-type key string)
-  (check-type list string)
-  (loop :for i :across list
-	:do (cond ((eq i #\() (incf depth))
-		  ((eq i #\)) (decf depth)))
-	:collect (list i depth) :into parsed-list
-	:when (and (eq i (elt key 0))
-		   (equal key (subseq list cnt (+ cnt key-len))))
-	  :collect (cons cnt depth) :into pos
-	:count i :into cnt
-	:finally (ignore-errors
-		  (loop :for (p . d) :in pos
-			:for first-part = (subseq parsed-list 0 p)
-			:for rest-part = (subseq parsed-list p)
-			:for key-depth = (- d skin)
-			:for open-pos = (position `(#\( ,key-depth) first-part :test #'equal :from-end t)
-			:for close-pos = (+ p (position `(#\) ,(1- key-depth)) rest-part :test #'equal) 1)
-			:collect (subseq list open-pos close-pos) :into total
-			:finally (if rm-dup
-				     (format t "~a" (remove-duplicates total :test #'equal))
-				     (format t "~a" total)))))) ; => SEEK
+  (let ((list (format nil "~(~a~)" list)))
+    (loop :for i :across list
+	  :do (cond ((eq i #\() (incf depth))
+		    ((eq i #\)) (decf depth)))
+	  :collect (list i depth) :into parsed-list
+	  :when (and (eq i (elt key 0))
+		     (equal key (subseq list cnt (+ cnt key-len))))
+	    :collect (cons cnt depth) :into pos
+	  :count i :into cnt
+	  :finally (ignore-errors
+		    (loop :for (p . d) :in pos
+			  :for first-part = (subseq parsed-list 0 p)
+			  :for rest-part = (subseq parsed-list p)
+			  :for key-depth = (- d skin)
+			  :for open-pos = (position `(#\( ,key-depth) first-part :test #'equal :from-end t)
+			  :for close-pos = (+ p (position `(#\) ,(1- key-depth)) rest-part :test #'equal) 1)
+			  :collect (subseq list open-pos close-pos) :into total
+			  :finally (if rm-dup
+				       (format t "~a" (remove-duplicates total :test #'equal))
+				       (format t "~a" total)))))))
+
 
 (defmacro seek-files (key &optional (buff) &rest files)
   `(check-type ,key string)
@@ -35,7 +36,7 @@
 	     (with-open-file (in f :direction :input)
 	       (loop
 		 (if (setq buff (read in nil))
-		     (format t "~(~s~)~%" buff)
+		     (format t "~(~a~)~%" buff)
 		     (return)))))))
     (seek ,key (with-output-to-string (*standard-output*)
 		  (cat-files ,@files))))) ; => SEEK-FILES
