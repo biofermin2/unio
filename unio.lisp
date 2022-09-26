34c50fd0a45f8aeacc4ebc29973799bc19cbae8e
biofermin2 <supercaveman@gmail.com>
2022-09-26 16:15:33 +0900

    fix replace function

--- unio.lisp
+++ unio.lisp
@@ -1,8 +1,9 @@
+;;(ql:quickload '(:cl-debug-print) :silent t) ; => (:CL-DEBUG-PRINT)
 (defpackage :unio                      
-  (:use :cl)
-  (:export :mappend :string-cat :set-readtable :depth-lst :peel :seek :cat :sets)) ; =>#<PACKAGE "UNIO"> 
+  (:use :cl)           
+  (:export :scan :%s :mappend :string-cat :set-readtable :depth-lst :peel :seek :cat :sets)) ; =>#<PACKAGE "UNIO"> 
 (in-package :unio)                      ; =>#<PACKAGE "UNIO"> 
-
+;;(debug-print:use-debug-print)           ; =>T 
 (defparameter depth-lst ())             ; =>DEPTH-LST 
 
 (defun parsed-list (l)
@@ -16,6 +17,7 @@
 (defun blank-p (c)
   (when (find c '(#\Space #\Return #\Newline #\| #\/ #\\))
     t))                                 ; =>BLANK-P 
+
 (defun paren-p (c)
   (when (find c '(#\( #\) #\[ #\] #\{ #\} #\< #\>))
     t))                                 ; =>PAREN-P 
@@ -38,19 +40,43 @@
      ,@body))                           ; =>SET-READTABLE 
 
 
+(defun scan (key obj)
+  (let* ((key-len (length key))
+         (result (search key obj))
+        (start (when result result)))
+    (if start
+        (values start (+ start key-len))
+        nil)))                          ; =>SCAN 
+
+(defun %s (old new obj)
+  (multiple-value-bind (start end)
+      (scan old obj)
+    (if start
+        (let* ((rest (subseq obj end))
+               (result (%s old new rest)))
+          (concatenate 'string (subseq obj 0 start) new result))
+        obj)))                          ; =>%S 
+
+
 (defun get-core (pl pos-lst &key (skin 0))
   (mapcar #'(lambda (x) (let* ((p (car x))
 			       (cdr-pl (mapcar #'cdr pl))
 			       (first-part (subseq cdr-pl 0 p))
 			       (rest-part (subseq cdr-pl p))
 			       (k-depth (- (third x) skin))
-			       (open-pos (position `(#\( ,k-depth) first-part :test #'equal :from-end t))
-			       (close-pos (+ p (position `(#\) ,(1- k-depth)) rest-part :test #'equal) 1)))
+			       (open-pos (handler-case (position `(#\( ,k-depth) first-part :test #'equal :from-end t)
+                                           (type-error (c))))
+			       (close-pos (handler-case (+ p (position `(#\) ,(1- k-depth)) rest-part :test #'equal) 1)
+                                            (type-error (c)))))
 			  (values (coerce (subseq (mapcar #'cadr pl) open-pos close-pos) 'string)
 				  (push k-depth depth-lst))))
 	  pos-lst))                     ; =>GET-CORE 
 
-;; objは文字列に固定 シンボルは負荷にする。[2022-09-02 22:46:34]
+(defmacro alambda (parms &body body)
+  `(labels ((self ,parms ,@body))
+     #'self))                           ; =>ALAMBDA 
+
+;;objは文字列に固定 シンボルの使用は不可にする。[2022-09-02 22:46:34]
 (defun seek (key obj &key (skin 0) (dup nil) (dep nil) (str t) (exact nil) (opt :preserve))
   (check-type key string)
   (check-type obj string)
@@ -76,9 +102,12 @@
       (setf depth-lst nil)
       (if str
 	  base
-	  (set-readtable opt (mapcar #'read-from-string base)))))) ; => 
+	  (set-readtable opt (mapcar #'read-from-string base)))))) ; =>SEEK 
+
+;;(hl-key (format nil "[31m~a[0m" key))
 
 (defun mappend (fn lst)
+  "from PAIP"
   (apply #'append (mapcar fn lst)))     ; =>MAPPEND 
 
 (defun cat (&rest files)
@@ -86,8 +115,7 @@
     (with-open-file (in f :direction :input)
       (loop :for line = (read-line in nil nil)
             :while line                         
-            :collect (format t "~a~&" line))))) ; =>CAT 
-
+            :collect (format t "~a~%" line))))) ; =>CAT 
 
 (defun string-cat (&rest files)
   (with-output-to-string (*standard-output*)
